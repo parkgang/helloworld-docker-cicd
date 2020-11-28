9e19a7defa1ef4aa023b4986e734e70346da061e
박경은 <63892989+ruddms936@users.noreply.github.com>
2020-11-29 05:54:33 +0900

    1

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-docker-compose -p test-blue -f docker-compose.blue.yml up -d
+DOCKER_APP_NAME=blue-green-server
 
-sleep 10
+EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
-docker-compose -p test-green -f docker-compose.green.yml down
+if [ -z "$EXIST_BLUE" ]; then
+	echo "blue up"
+	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d
+
+	sleep 10
+
+	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+else
+	echo "green up"
+	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d
+
+	sleep 10
+
+	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+fi
