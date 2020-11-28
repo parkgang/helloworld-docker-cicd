e38fb731bfaad0b4468de484e3a7130fd50569c2
박경은 <63892989+ruddms936@users.noreply.github.com>
2020-11-29 05:44:49 +0900

    1

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-EXIST_BLUE=$(docker-compose -p test-blue -f docker-compose.blue.yml ps | grep Up)
+docker-compose -p test-blue -f docker-compose.blue.yml up -d
 
-if [ -z "$EXIST_BLUE" ]; then
-	echo "blue up"
-	docker-compose -p test-blue -f docker-compose.blue.yml up -d
+sleep 10
 
-	sleep 10
-
-	docker-compose -p test-green -f docker-compose.green.yml down
-else
-	echo "green up"
-	docker-compose -p test-green -f docker-compose.green.yml up -d
-
-	sleep 10
-
-	docker-compose -p test-blue -f docker-compose.blue.yml down
-fi
+docker-compose -p test-green -f docker-compose.green.yml down
